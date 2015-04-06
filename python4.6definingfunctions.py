a8ffa4eeb02648b371296734cc796f22f21584ac
Lablo <topher.hoang@gmail.com>
2015-04-06 11:56:58 -0400

    End of day April 6, 2015

--- python4.6definingfunctions.py
+++ python4.6definingfunctions.py
@@ -1,6 +1,6 @@
 #Defining functions! The indentation pattern for functions is the same
 #as loops, if statements. One difference is the first line can be a
-#Documentation String that is read by document producing tools.
+#documentation String that is read by document producing tools.
 def fibby(n):
     "This is a fibonacci function"
     a,b = 0,1
@@ -10,7 +10,7 @@ def fibby(n):
     #print()
 
 #A function that returns the value instead of printing it.
-#By default a function returns None even if it isn't defined anywhere.
+#By default a function returns None even if a return value isn't defined anywhere.
 def fibbyreturns(n):
     "This is a fibonacci function"
     result=[]
@@ -58,3 +58,65 @@ except OSError:
     print('You used all the retries!')
 
 
+#Default values defined within a function are evaluated or assigned its value
+#at point the function is defined. IF the variable is assigned a new variable
+#after the function has been defined. The variable within the function
+#will have the value before it was redefined.
+i=5
+def f(b,a=i):
+    c = a + b
+    print('The value of a is',a)
+    a = a + b
+    print('The value of c defined in the function is',c)
+i=90
+
+#Note that the function modifies a but the value of a is not retained
+#between function calls. This is due to the scope of the variable and that its
+#not mutable. 
+f(3)
+print('')
+f(4)
+
+#If a new value for args is assigned when the function is called, it will accept it.
+#f(10)
+
+#If the value of i is changed it will not update the default value of arg in the function.
+#i=100
+#f(1000)
+
+#If one of the argument variables is a mutable object like
+#a list, dictionary or the values of the mutable object will be retained.
+#It will NOT revert to its default.
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+
+#If a list is passed to the function, the function will not set the list to empty.
+#Mutable objects 
+#The default value of a list can be reset using the following statements.
+
+def g(b, L=None):
+    if L == None:
+        L=[]
+    L.append(b)
+    return L
+print('')
+thelist = g(1)
+print('No list passed to g()')
+print(g(1))
+print('Calling function g(1,thelist) and passing thelist to it')
+print(g(1,thelist))
+print('Assigning list multiple values but not passing it to g(5)')
+thelist = [1,2,3,4]
+print(g(5))
+print('Calling g(5,thelist) but passing thelist')
+print(g(5,thelist))
+
+
+#Keyword Arguments
+#
